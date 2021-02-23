605808be2eceeb04c5ec93d4f6b9b389aeaf65a4
Alexenfeu <alexissarlat@gmail.com>
2021-02-23 19:29:51 +0100

    initial project commit

---
+++ Heritage_Interfaces_Save/Heritage_Interfaces_Save/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage_Interfaces_Save
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
